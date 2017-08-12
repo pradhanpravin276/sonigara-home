15484db0dfb0a7980ac188afc7c10106c8a092d1
pravin <pradhan.pravin276@gmail.com>
2017-08-12 07:17:57 +0530

    sonigara web-app

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sonigaraWebAppApp
+ * @description
+ * # sonigaraWebAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sonigaraWebAppApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sonigaraWebAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sonigaraWebAppApp
+ */
+angular.module('sonigaraWebAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
